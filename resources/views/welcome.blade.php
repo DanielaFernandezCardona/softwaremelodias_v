dcdd24f12bb58fabc7bc48d444696cd0e339e2f5
DanielaFernandezCardona <danielita.2795@hotmail.com>
2018-04-08 21:32:12 -0500

    Página de inicio
    Se construye la página de inicio de la aplicación.

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,95 +1,70 @@
-<!doctype html>
-<html lang="{{ app()->getLocale() }}">
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+<link href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+<script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/js/bootstrap.min.js"></script>
+<script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
+<!------ Include the above in your HEAD tag ---------->
 
-        <title>Laravel</title>
+<!-- REFERENCIAS A LOS ARCHIVOS .CSS Y .JS -->
+<link rel="stylesheet" type="text/css" href=".../public/css/welcome.css">
+<script href=".../public/js/welcome.js"></script>
 
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
+<!-- ////////////////////////////////////////////////////////////////////////// -->
 
-        <!-- Styles -->
-        <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Raleway', sans-serif;
-                font-weight: 100;
-                height: 100vh;
-                margin: 0;
-            }
-
-            .full-height {
-                height: 100vh;
-            }
-
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
-
-            .position-ref {
-                position: relative;
-            }
-
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
-
-            .content {
-                text-align: center;
-            }
-
-            .title {
-                font-size: 84px;
-            }
-
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 12px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
-
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-        </style>
-    </head>
-    <body>
-        <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @auth
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ route('login') }}">Login</a>
-                        <a href="{{ route('register') }}">Register</a>
-                    @endauth
-                </div>
-            @endif
-
-            <div class="content">
-                <div class="title m-b-md">
-                    Laravel
-                </div>
-
-                <div class="links">
-                    <a href="https://laravel.com/docs">Documentation</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
-                </div>
-            </div>
+<!--
+more ui & layout https://github.com/alphadsy
+contribute on github : https://github.com/alphadsy/alpha-ui
+-->
+<nav class="navbar navbar-default navbar-doublerow navbar-trans navbar-fixed-top">
+  <!-- top nav -->
+  <nav class="navbar navbar-top hidden-xs">
+    <div class="container">
+      <!-- left nav top -->
+      <ul class="nav navbar-nav pull-left">
+        <li><a href="#"><span class="glyphicon glyphicon-thumbs-up text-white"></span></a></li>
+        <li><a href="#"><span class="glyphicon glyphicon-globe text-white"></span></a></li>
+        <li><a href="#"><span class="glyphicon glyphicon-pushpin text-white"></span></a></li>
+        <li><a href="#"><span class="text-white">QUESTIONS? CALL: <b>+963000000000</b></span></a></li>
+      </ul>
+      <!-- right nav top -->
+      <ul class="nav navbar-nav pull-right">
+        <li><a href="https://github.com/alphadsy" class="text-white">About Us</a></li>
+        <li><a href="https://github.com/alphadsy" class="text-white">Contact Us</a></li> 
+      </ul>
+    </div>
+    <div class="dividline light-grey"></div>
+  </nav>
+  <!-- down nav -->
+  <nav class="navbar navbar-down">
+    <div class="container">
+      <div class="flex-container">  
+        <div class="navbar-header flex-item">
+          <div class="navbar-brand" href="https://github.com/alphadsy">ALPHAD</div>
         </div>
-    </body>
-</html>
+        <ul class="nav navbar-nav flex-item hidden-xs">
+          <li><a href="https://github.com/alphadsy">Projects</a></li>
+          <li><a href="https://github.com/alphadsy">Pricing</a></li> 
+          <li><a href="https://github.com/shu3aybbadran">TheTeam</a></li> 
+        </ul>
+        <ul class="nav navbar-nav flex-item hidden-xs pull-right">
+          <li><a href="https://bootsnipp.com/snippets/M5Nyj" class="">Header sectoin!</a></li> 
+        </ul>
+        <!-- dropdown only moblie -->
+          <div class="dropdown visible-xs pull-right">
+            <button class="btn btn-default dropdown-toggle " type="button" id="dropdownmenu" data-toggle="dropdown">
+              <span class="glyphicon glyphicon-align-justify"></span> 
+            </button>
+            <ul class="dropdown-menu">
+              <li><a href="#">Projects</a></li>
+              <li><a href="#">Pricing</a></li> 
+              <li><a href="#">TheTeam</a></li> 
+              <li role="separator" class="divider"></li>
+              <li><a href="#">contact us</a></li>
+            </ul>
+          </div>
+        </div>  
+      </div>
+    </nav>
+  </nav> 
+<!--bg img  -->
+<header>
+    <img src="http://todofondos.com/bin/fondos/07/59/17d.jpg" style="width:100%">
+</header>    
